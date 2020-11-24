d37e410fc21dc7620a2024e29335213a5b988d3f
devops.maksim.p <devops.maksim.p@alldigitalads.com>
2020-11-24 14:49:49 +0200

    add trimpath

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 export CGO_ENABLED=0
 export GO111MODULE=on
 export TAGS=""
-export GOFLAGS=""
+export GOFLAGS="-trimpath"
 export LDFLAGS="-X main.buildTime=`date +\"%Y%m%d%H%M%S\"` -X main.buildGitTag=`git describe --exact-match --tags $(git log -n1 --pretty='%h')`"
 export TARGETS="darwin/amd64 linux/amd64"
 export BINNAME="file-sync"
@@ -11,4 +11,4 @@ export GOX="go run github.com/mitchellh/gox"
 
 rm -rf _dist
 
-$GOX -parallel=3 -output="_dist/$BINNAME-{{.OS}}-{{.Arch}}" -osarch="$TARGETS" $GOFLAGS -tags "$TAGS" -ldflags "$LDFLAGS" ./cmd/
+$GOX -parallel=3 -output="_dist/$BINNAME-{{.OS}}-{{.Arch}}" -osarch="$TARGETS" -tags "$TAGS" -ldflags "$LDFLAGS" ./cmd/
